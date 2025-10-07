b3d36a70875dd4c871289bd3d50d803afbb2f07a
Slusser <slusarekjakub@gmail.com>
2025-10-07 20:30:21 +0200

    feature: swagger

--- apps/api/src/app/app.controller.ts
+++ apps/api/src/app/app.controller.ts
@@ -1,11 +1,26 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @ApiOperation({ summary: 'Pobierz dane aplikacji' })
+  @ApiResponse({ 
+    status: 200, 
+    description: 'Dane zostały pomyślnie pobrane',
+    schema: {
+      type: 'object',
+      properties: {
+        message: {
+          type: 'string',
+          example: 'Hello API'
+        }
+      }
+    }
+  })
   getData() {
     return this.appService.getData();
   }
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -6,15 +6,30 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
+
+  const config = new DocumentBuilder()
+  .setTitle('Sales Analysis API')
+  .setDescription('API dla aplikacji analizy sprzedaży')
+  .setVersion('1.0')
+  .addTag('sales')
+  .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3000;
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+  );
+  Logger.log(
+    `📚 Swagger documentation is available on: http://localhost:${port}/api/docs`
   );
 }
 
