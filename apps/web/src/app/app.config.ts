a8a12a60f016f54d01689913511c7ec6d643e2f7
Slusser <slusarekjakub@gmail.com>
2025-10-13 22:52:13 +0200

    fix: zorro icons

--- apps/web/src/app/app.config.ts
+++ apps/web/src/app/app.config.ts
@@ -6,9 +6,21 @@ import {
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import {
+  provideHttpClient,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { NzIconModule } from 'ng-zorro-antd/icon';
+import { provideNzIcons } from 'ng-zorro-antd/icon';
+import {
+  ReloadOutline,
+  SearchOutline,
+  CloseOutline,
+  RollbackOutline,
+  EditOutline,
+  DeleteOutline,
+  PlusOutline
+} from '@ant-design/icons-angular/icons';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -17,5 +29,15 @@ export const appConfig: ApplicationConfig = {
     provideHttpClient(withInterceptorsFromDi()),
     provideAnimations(),
     provideRouter(appRoutes),
+    provideNzIcons([
+      ReloadOutline,
+      SearchOutline,
+      CloseOutline,
+      RollbackOutline,
+      EditOutline,
+      DeleteOutline,
+      PlusOutline,
+      
+    ]),
   ],
 };
